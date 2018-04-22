6e2a9f71478187f0a1c5740379c1786ebe2b54de
Dariusz Szymczyk <edarek@op.pl>
2018-04-22 16:37:19 +0200

    Users controller created.

---
+++ server/controllers/users.js
@@ -0,0 +1,81 @@
+const User = require('../models').User;
+const sequelize = require('sequelize');
+const errorResponder = require('./../utils/errorResponder');
+const successResponder = require('./../utils/successResponder');
+
+module.exports = {
+    getAll(req, res) {
+        return User
+            .count()
+            .then((amount) => {
+                return User
+                    .findAll({
+                        attributes: ['id', 'email', 'role', 'createdAt', 'updatedAt'],
+                        order: [
+                            ['createdAt', 'DESC'],
+                        ],
+                        offset: req.offset,
+                        limit: req.limit
+                    })
+                    .then((users) => res.status(200).send(successResponder(users, amount)))
+                    .catch((error) => res.status(400).send(errorResponder(error)));
+            })
+            .catch((error) => res.status(400).send(errorResponder(error)));
+    },
+
+    get(req, res) {
+        return User
+            .findById(req.params.id, {
+                attributes: ['id', 'email', 'role', 'createdAt', 'updatedAt'],
+            })
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).send(errorResponder({
+                        message: 'User not found.',
+                    }));
+                }
+                return res.status(200).send(successResponder(user));
+            })
+            .catch((error) => res.status(400).send(errorResponder(error)));
+    },
+
+    update(req, res) {
+        return User
+            .findById(req.params.id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send(errorResponder({
+                        message: 'User not found.',
+                    }));
+                }
+
+                let role = req.body.role;
+
+                return user
+                    .update({
+                        role: role
+                    })
+                    .then(updatedUser => res.status(200).send(successResponder(updatedUser)))
+                    .catch(error => res.status(400).send(errorResponder(error)));
+            })
+            .catch(error => res.status(400).send(errorResponder(error)));
+    },
+
+    destroy(req, res) {
+        return User
+            .findById(req.params.id)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).send(errorResponder({
+                        message: 'User not found.',
+                    }));
+                }
+
+                return user
+                    .destroy()
+                    .then(() => res.status(204).send(successResponder(true)))
+                    .catch(error => res.status(400).send(errorResponder(error)));
+            })
+            .catch(error => res.status(400).send(error));
+    }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,62 @@
 const authController = require('../controllers/auth');
+const controllers = require('../controllers');
 const passport = require("passport");
 const { checkSchema } = require('express-validator/check');
 const schemas = require("./../validation/schemas");
-const errorHandler = require("./../validation/errorHandler");
+const schemaErrorHandler = require("./../validation/errorHandler");
 
 module.exports = (app) => {
     app.get('/api', (req, res) => res.status(200).send({
-        message: 'Welcome to the Todos API!!!',
+        message: 'Welcome to the Blog API!!!',
     }));
 
-    app.post('/api/auth/facebook', checkSchema(schemas.login), errorHandler, passport.authenticate('facebook-token', { session: false }), authController.prepareUser, authController.generateToken, authController.sendToken);
+    // Authentication
+    app.post('/api/auth/facebook', checkSchema(schemas.login), schemaErrorHandler, passport.authenticate('facebook-token', { session: false }), authController.prepareUser, authController.generateToken, authController.sendToken);
     // app.get('/api/auth/me', authController.authenticate, authController.getCurrentUser);
+
+    // Users
+    // Get all users
+    app.get('/api/users', controllers.users.getAll);
+
+    // Get user
+    app.get('/api/users/:id', controllers.users.get);
+
+    // Edit user
+    app.put('/api/users/:id', controllers.users.update);
+
+    // Delete user
+    // app.delete('/api/users/:id', authController.authenticate);
+
+    // Posts
+    // Get all my posts including following users posts
+    // app.get('/api/posts', authController.authenticate);
+
+    // Get user posts
+    // app.get('/api/posts/:userId', authController.authenticate);
+
+    // Add post
+    // app.post('/api/posts', authController.authenticate);
+
+    // Edit post
+    // app.put('/api/posts/:id', authController.authenticate);
+
+    // Delete post
+    // app.delete('/api/posts/:id', authController.authenticate);
+
+    // Comments
+    // Get post comments
+    // app.get('/api/comments/:postId', authController.authenticate);
+
+    // Add comment to post
+    // app.post('/api/comments/:postId', authController.authenticate);
+
+    // Edit comment
+    // app.put('/api/comments/:id', authController.authenticate);
+
+    // Delete comment
+    // app.delete('/api/comments/:id', authController.authenticate);
+
+    // Follows
+    // Get following users
+    // app.get('/api/follows', authController.authenticate);
 };
