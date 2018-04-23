aedff71ef7a02b0e1dda77e3217cf4267d2bd07e
Dariusz Szymczyk <edarek@op.pl>
2018-04-23 22:31:17 +0200

    Comments tests created.

--- test/test.js
+++ test/test.js
@@ -20,8 +20,8 @@ chai.use(chaiHttp);
 describe('Blog', function () {
     let user1_token;
     let user2_token;
-    
-    before(function() {
+
+    before(function () {
         // Create token
         user1_token = authController.createToken({
             id: 1,
@@ -66,184 +66,253 @@ describe('Blog', function () {
     describe('Users', function () {
         it('Should return non empty users list', function (done) {
             chai.request(server)
-            .get('/api/users')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(200);
+                .get('/api/users')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                res.body.should.have.property("amount");
-                res.body.amount.should.be.a("number");
-                res.body.amount.should.equal(3);
+                    res.body.should.have.property("amount");
+                    res.body.amount.should.be.a("number");
+                    res.body.amount.should.equal(3);
 
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("array");
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("array");
 
-                done();
-            });
+                    done();
+                });
         });
 
         it('Should return user details', function (done) {
             chai.request(server)
-            .get('/api/users/1')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(200);
+                .get('/api/users/1')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("object");
-                
-                res.body.data.should.have.property("id");
-                res.body.data.id.should.be.a("number");
-                res.body.data.id.should.equal(1);
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
 
-                done();
-            });
+                    res.body.data.should.have.property("id");
+                    res.body.data.id.should.be.a("number");
+                    res.body.data.id.should.equal(1);
+
+                    done();
+                });
         });
-        
+
         it('Should edit user and return updated details', function (done) {
             chai.request(server)
-            .put('/api/users/3')
-            .set('x-auth-token', user1_token)
-            .send({ role: 'user' })
-            .end(function (err, res) {
-                res.should.have.status(200);
-                
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("object");
+                .put('/api/users/3')
+                .set('x-auth-token', user1_token)
+                .send({ role: 'user' })
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                res.body.data.should.have.property("role");
-                res.body.data.role.should.be.a("string");
-                res.body.data.role.should.equal("user");
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
 
-                done();
-            });
+                    res.body.data.should.have.property("role");
+                    res.body.data.role.should.be.a("string");
+                    res.body.data.role.should.equal("user");
+
+                    done();
+                });
         });
-        
+
         it('Should delete user', function (done) {
             chai.request(server)
-            .delete('/api/users/3')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(204);
+                .delete('/api/users/3')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(204);
 
-                done();
-            });
+                    done();
+                });
         });
-        
+
         it('Should return error while deleting user again, because user doesn\'t exist', function (done) {
             chai.request(server)
-            .delete('/api/users/3')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(404);
+                .delete('/api/users/3')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(404);
 
-                done();
-            });
+                    done();
+                });
         });
     });
 
     describe('Posts', function () {
         it('Should add post', function (done) {
             chai.request(server)
-            .post('/api/posts')
-            .set('x-auth-token', user1_token)
-            .send({ text: 'Post 1' })
-            .end(function (err, res) {
-                res.should.have.status(201);
-                    
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("object");
+                .post('/api/posts')
+                .set('x-auth-token', user1_token)
+                .send({ text: 'Post 1' })
+                .end(function (err, res) {
+                    res.should.have.status(201);
 
-                res.body.data.should.have.property("text");
-                res.body.data.text.should.be.a("string");
-                res.body.data.text.should.equal("Post 1");
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
 
-                done();
-            });
+                    res.body.data.should.have.property("text");
+                    res.body.data.text.should.be.a("string");
+                    res.body.data.text.should.equal("Post 1");
+
+                    done();
+                });
         });
 
         it('Should return non empty user posts list', function (done) {
             chai.request(server)
-            .get('/api/posts')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(200);
+                .get('/api/posts')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                res.body.should.have.property("amount");
-                res.body.amount.should.be.a("number");
-                res.body.amount.should.equal(1);
+                    res.body.should.have.property("amount");
+                    res.body.amount.should.be.a("number");
+                    res.body.amount.should.equal(1);
 
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("array");
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("array");
 
-                done();
-            });
+                    done();
+                });
         });
 
         it('Should edit post', function (done) {
             chai.request(server)
-            .put('/api/posts/1')
-            .set('x-auth-token', user1_token)
-            .send({ text: 'Post 2' })
-            .end(function (err, res) {
-                res.should.have.status(200);
-                
-                res.body.should.have.property("data");
-                res.body.data.should.be.an("object");
+                .put('/api/posts/1')
+                .set('x-auth-token', user1_token)
+                .send({ text: 'Post 2' })
+                .end(function (err, res) {
+                    res.should.have.status(200);
 
-                res.body.data.should.have.property("text");
-                res.body.data.text.should.be.a("string");
-                res.body.data.text.should.equal("Post 2");
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
 
-                done();
-            });
+                    res.body.data.should.have.property("text");
+                    res.body.data.text.should.be.a("string");
+                    res.body.data.text.should.equal("Post 2");
+
+                    done();
+                });
         });
 
         it('Should delete post', function (done) {
             chai.request(server)
-            .delete('/api/posts/1')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(204);
+                .delete('/api/posts/1')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(204);
 
-                done();
-            });
+                    done();
+                });
         });
-        
+
         it('Should return error while deleting post again, because post doesn\'t exist', function (done) {
             chai.request(server)
-            .delete('/api/posts/1')
-            .set('x-auth-token', user1_token)
-            .end(function (err, res) {
-                res.should.have.status(404);
+                .delete('/api/posts/1')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(404);
 
-                done();
-            });
+                    done();
+                });
         });
     });
-    
-    // describe('Comments', function () {
-    //     it('Should add comment to post', function (done) {
 
-    //     });
+    describe('Comments', function () {
+        before(function (done) {
+            chai.request(server)
+                .post('/api/posts')
+                .set('x-auth-token', user1_token)
+                .send({ text: 'Post 2' })
+                .end(function (err, res) {
+                    done();
+                });
+        });
 
-    //     it('Should return non empty post comments list', function (done) {
+        it('Should add comment to post', function (done) {
+            chai.request(server)
+                .post('/api/comments/2')
+                .set('x-auth-token', user1_token)
+                .send({ text: 'Comment 1' })
+                .end(function (err, res) {
+                    res.should.have.status(201);
 
-    //     });
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
 
-    //     it('Should edit comment', function (done) {
+                    res.body.data.should.have.property("text");
+                    res.body.data.text.should.be.a("string");
+                    res.body.data.text.should.equal("Comment 1");
 
-    //     });
+                    done();
+                });
+        });
 
-    //     it('Should delete comment', function (done) {
+        it('Should return non empty post comments list', function (done) {
+            chai.request(server)
+                .get('/api/comments/2')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    console.log(res.body.data);
+                    res.should.have.status(200);
 
-    //     });
-        
-    //     it('Should return error while deleting comment again, because comment doesn\'t exist', function (done) {
-            
-    //     });
-    // });
-    
+                    res.body.should.have.property("amount");
+                    res.body.amount.should.be.a("number");
+                    res.body.amount.should.equal(1);
+
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("array");
+
+                    done();
+                });
+        });
+
+        it('Should edit comment', function (done) {
+            chai.request(server)
+                .put('/api/comments/1')
+                .set('x-auth-token', user1_token)
+                .send({ text: 'Comment 2' })
+                .end(function (err, res) {
+                    res.should.have.status(200);
+
+                    res.body.should.have.property("data");
+                    res.body.data.should.be.an("object");
+
+                    res.body.data.should.have.property("text");
+                    res.body.data.text.should.be.a("string");
+                    res.body.data.text.should.equal("Comment 2");
+
+                    done();
+                });
+        });
+
+        it('Should delete comment', function (done) {
+            chai.request(server)
+                .delete('/api/comments/1')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(204);
+
+                    done();
+                });
+        });
+
+        it('Should return error while deleting comment again, because comment doesn\'t exist', function (done) {
+            chai.request(server)
+                .delete('/api/comments/1')
+                .set('x-auth-token', user1_token)
+                .end(function (err, res) {
+                    res.should.have.status(404);
+
+                    done();
+                });
+        });
+    });
+
     // describe('Follows', function () {
     //     it('Should add follow', function (done) {
 
@@ -264,9 +333,9 @@ describe('Blog', function () {
     //     it('Should delete follow', function (done) {
 
     //     });
-        
+
     //     it('Should return error while deleting follow again, because follow doesn\'t exist', function (done) {
-            
+
     //     });
     // });
 });
